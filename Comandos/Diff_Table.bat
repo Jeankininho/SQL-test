5047944965f387fda80092179dd811f437f5765e
Jeanderson Machado <jean.machado@skysoftware.com.br>
2022-02-22 18:32:52 -0300

    Inserindo dados

--- Comandos/Diff_Table.bat
+++ Comandos/Diff_Table.bat
@@ -1,4 +1,4 @@
 cd /d D:\SQLGit
 cd D:\SQLGit\Comandos
-git diff D:\SQLGit\Tables\ChangeEvent.sql > ChangeEvent_Log_22-02-2022_15-24-50.txt
+git diff D:\SQLGit\Tables\ChangeEvent.sql > ChangeEvent_Log_22-02-2022_18-32-51.txt
 exit
